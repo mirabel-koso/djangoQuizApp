33cf2a907e2efdb806e44d7ad29a0005b856be07
Mirabel Ekwenugo <mirabel.ekwenugo@andela.co>
2016-06-20 17:44:10 +0100

    add css

--- djangoQuiz/settings.py
+++ djangoQuiz/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/main/static/'
 
 
 ACCOUNT_USERNAME_REQUIRED = False
