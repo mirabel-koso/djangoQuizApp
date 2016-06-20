76a8385eb4fa3bd87348d5011a1b130194817680
Mirabel Ekwenugo <mirabel.ekwenugo@andela.co>
2016-06-20 14:05:57 +0100

    create models for user detail

--- main/models.py
+++ main/models.py
@@ -59,22 +59,15 @@
 #         return ", ".join([question.text for question in self.questions.all()])
 
 from mongoengine import *
-from mongoengine.fields import ReferenceField
+from mongoengine.fields import SequenceField
 from djangoQuiz.settings import DBNAME
 
 connect(DBNAME)
 
 
-class Quiz(Document):
-    """docstring for Quiz."""
+class Options(EmbeddedDocument):
+    """docstring for Options."""
 
-    name = StringField(max_length=200, required=True)
-    # question = ReferenceField('Question', reverse_delete_rule=0)
-    question = ReferenceField('Question', reverse_delete_rule=0)
-    # pass_mark = IntField(min_value=0, max_value=100)
-
-
-class Options(Document):
     option_one = StringField(max_length=600, required=True)
     option_two = StringField(max_length=600, required=True)
     option_three = StringField(max_length=600, required=True)
@@ -82,8 +75,32 @@ class Options(Document):
     option_five = StringField(max_length=600, required=True)
 
 
-class Question(Document):
+class Question(EmbeddedDocument):
     """docstring for Answers."""
+
+    QID = SequenceField()
     question_text = StringField(max_length=600, required=True)
-    options = ReferenceField('Options', reverse_delete_rule=0)
+    # options = ReferenceField('Options', reverse_delete_rule=0)
+    options = ListField(EmbeddedDocumentField(Options))
     answer = StringField(max_length=200, required=True)
+
+
+class Quiz(Document):
+    """docstring for Quiz."""
+
+    name = StringField(max_length=200, required=True)
+    question = ListField(EmbeddedDocumentField(Question))
+    # question = ReferenceField('Question', reverse_delete_rule=0)
+    # pass_mark = IntField(min_value=0, max_value=100)
+
+class Detail(EmbeddedDocument):
+    score = IntField(max_length=200, required=True)
+    course_name = StringField(max_length=200, required=True)
+
+
+class UserDetails(Document):
+    """docstring for Quiz."""
+
+    username = StringField(max_length=200, required=True)
+    user_detail = ListField(EmbeddedDocumentField(Detail))
+
